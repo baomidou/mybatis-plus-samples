22d0de2731c6754066011fed6557ef356774774c
miemie <wonderming@vip.qq.com>
2018-08-11 19:39:07 +0800

    测试

--- mybatis-plus-sample-pagination/src/main/java/com/baomidou/mybatisplus/samples/pagination/mapper/UserMapper.java
+++ mybatis-plus-sample-pagination/src/main/java/com/baomidou/mybatisplus/samples/pagination/mapper/UserMapper.java
@@ -5,8 +5,6 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.samples.pagination.entity.User;
 import com.baomidou.mybatisplus.samples.pagination.model.MyPage;
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.ResultType;
-import org.apache.ibatis.annotations.Select;
 
 /**
  * @author miemie
@@ -15,13 +13,13 @@ import org.apache.ibatis.annotations.Select;
 public interface UserMapper extends BaseMapper<User> {
 
     /**
-     * 3.x 的 page 可以进行取值,多入参记得加上注解
+     * 3.x 的 page 可以进行取值,多个入参记得加上注解
+     * 自定义 page 类必须放在入参第一位
+     * 返回值可以用 IPage<T> 接收 也可以使用入参的 MyPage<T> 接收
      * todo 目前使用注解会报错,写在 xml 里就没事
      *
      * @param myPage 自定义 page
      * @return 分页数据
      */
-    @Select("select * from user where age = #{pg.selectInt} and name = #{pg.selectStr}")
-    @ResultType(User.class)
     IPage<User> mySelectPage(@Param("pg") MyPage<User> myPage);
 }
