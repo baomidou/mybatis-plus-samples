2b68419c1e9807462a0542ad6273f880930387bd
yuxiaobin <179634696@qq.com>
2018-12-26 10:32:03 +0800

    add: add Sequence test for MVC #628 github

--- mybatis-plus-sample-quickstart-springmvc/src/test/java/com/baomidou/mybatisplus/samples/quickstart/springmvc/SampleTest.java
+++ mybatis-plus-sample-quickstart-springmvc/src/test/java/com/baomidou/mybatisplus/samples/quickstart/springmvc/SampleTest.java
@@ -1,7 +1,7 @@
 package com.baomidou.mybatisplus.samples.quickstart.springmvc;
 
-import com.baomidou.mybatisplus.samples.quickstart.springmvc.entity.User;
-import com.baomidou.mybatisplus.samples.quickstart.springmvc.mapper.UserMapper;
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,7 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.util.List;
+import com.baomidou.mybatisplus.samples.quickstart.springmvc.entity.User;
+import com.baomidou.mybatisplus.samples.quickstart.springmvc.mapper.UserMapper;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath*:spring.xml"})
@@ -24,6 +25,21 @@ public class SampleTest {
         List<User> userList = userMapper.selectList(null);
         Assert.assertEquals(5, userList.size());
         userList.forEach(System.out::println);
+    }
+
+    @Test
+    public void testSequence() {
+        User u = new User();
+        u.setName("Tomcat");
+        u.setAge(18);
+        u.setEmail("kellylake@qq.com");
+        userMapper.insert(u);
+
+        Long id1 = u.getId();
+        u = new User();
+        u.setName("Tomcat2");
+        userMapper.insert(u);
+        Assert.assertEquals("id should increase 1", id1 + 1, u.getId().longValue());
     }
 
 }
