4a8aa083ef93c6ae39d210e7a5d606f126a5038d
miemieYaho <wonderming@vip.qq.com>
2019-03-22 19:40:40 +0800

    提交一下

--- mybatis-plus-sample-pagination/src/main/java/com/baomidou/mybatisplus/samples/pagination/mapper/UserMapper.java
+++ mybatis-plus-sample-pagination/src/main/java/com/baomidou/mybatisplus/samples/pagination/mapper/UserMapper.java
@@ -1,10 +1,11 @@
 package com.baomidou.mybatisplus.samples.pagination.mapper;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.samples.pagination.entity.User;
 import com.baomidou.mybatisplus.samples.pagination.model.MyPage;
 import com.baomidou.mybatisplus.samples.pagination.model.ParamSome;
-import org.apache.ibatis.annotations.Param;
 
 /**
  * @author miemie
@@ -16,10 +17,12 @@ public interface UserMapper extends BaseMapper<User> {
      * 3.x 的 page 可以进行取值,多个入参记得加上注解
      * 自定义 page 类必须放在入参第一位
      * 返回值可以用 IPage<T> 接收 也可以使用入参的 MyPage<T> 接收
-     * todo 目前使用注解会报错,写在 xml 里就没事
+     * <li> 3.1.0 之前的版本使用注解会报错,写在 xml 里就没事 </li>
+     * <li> 3.1.0 开始支持注解,但是返回值只支持 IPage ,不支持 IPage 的子类</li>
      *
      * @param myPage 自定义 page
      * @return 分页数据
      */
+//    @Select("select * from user where (age = #{pg.selectInt} and name = #{pg.selectStr}) or (age = #{ps.yihao} and name = #{ps.erhao})")
     MyPage<User> mySelectPage(@Param("pg") MyPage<User> myPage, @Param("ps") ParamSome paramSome);
 }
--- mybatis-plus-sample-pagination/src/test/java/com/baomidou/mybatisplus/samples/pagination/PaginationTest.java
+++ mybatis-plus-sample-pagination/src/test/java/com/baomidou/mybatisplus/samples/pagination/PaginationTest.java
@@ -1,5 +1,17 @@
 package com.baomidou.mybatisplus.samples.pagination;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.CollectionUtils;
+
 import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
@@ -8,16 +20,8 @@ import com.baomidou.mybatisplus.samples.pagination.entity.User;
 import com.baomidou.mybatisplus.samples.pagination.mapper.UserMapper;
 import com.baomidou.mybatisplus.samples.pagination.model.MyPage;
 import com.baomidou.mybatisplus.samples.pagination.model.ParamSome;
-import ikidou.reflect.TypeBuilder;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.util.CollectionUtils;
 
-import javax.annotation.Resource;
-import java.util.List;
+import ikidou.reflect.TypeBuilder;
 
 /**
  * @author miemie
@@ -36,7 +40,7 @@ public class PaginationTest {
         Page<User> page = new Page<>(1, 5);
         IPage<User> userIPage = mapper.selectPage(page, new QueryWrapper<User>()
                 .eq("age", 20).eq("name", "Jack"));
-        Assert.assertSame(page, userIPage);
+        assertThat(page).isSameAs(userIPage);
         System.out.println("总条数 ------> " + userIPage.getTotal());
         System.out.println("当前页数 ------> " + userIPage.getCurrent());
         System.out.println("当前每页显示数 ------> " + userIPage.getSize());
@@ -53,7 +57,7 @@ public class PaginationTest {
         MyPage<User> myPage = new MyPage<User>(1, 5).setSelectInt(20).setSelectStr("Jack");
         ParamSome paramSome = new ParamSome(20, "Jack");
         MyPage<User> userMyPage = mapper.mySelectPage(myPage, paramSome);
-        Assert.assertSame(myPage, userMyPage);
+        assertThat(myPage).isSameAs(userMyPage);
         System.out.println("总条数 ------> " + userMyPage.getTotal());
         System.out.println("当前页数 ------> " + userMyPage.getCurrent());
         System.out.println("当前每页显示数 ------> " + userMyPage.getSize());
