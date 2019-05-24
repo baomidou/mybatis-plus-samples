36b664e3e44d2097b2f052fb19dd581a64b97860
miemie <wonderming@vip.qq.com>
2019-05-24 12:36:40 +0800

    移除模块

--- mybatis-plus-sample-cascade/src/main/java/com/devloper/joker/mybatisplus/cascade/mapper/RoleMapper.java
+++
@@ -1,7 +0,0 @@
-package com.devloper.joker.mybatisplus.cascade.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.devloper.joker.mybatisplus.cascade.entity.Role;
-
-public interface RoleMapper extends BaseMapper<Role> {
-}
