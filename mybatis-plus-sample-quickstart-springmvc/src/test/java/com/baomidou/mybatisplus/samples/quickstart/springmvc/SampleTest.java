a01c9db52b2a1d4f9a60cdd1b5afc82dfb577392
yuxiaobin <179634696@qq.com>
2019-06-27 09:26:41 +0800

    add: pagination/mapper.xml sample for Spring MVC

--- mybatis-plus-sample-quickstart-springmvc/src/test/java/com/baomidou/mybatisplus/samples/quickstart/springmvc/SampleTest.java
+++ mybatis-plus-sample-quickstart-springmvc/src/test/java/com/baomidou/mybatisplus/samples/quickstart/springmvc/SampleTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.samples.quickstart.springmvc.entity.User;
 import com.baomidou.mybatisplus.samples.quickstart.springmvc.mapper.UserMapper;
 
@@ -40,6 +41,22 @@ public class SampleTest {
         u.setName("Tomcat2");
         userMapper.insert(u);
         Assert.assertEquals("id should increase 1", id1 + 1, u.getId().longValue());
+    }
+
+    @Test
+    public void testCustomizedSql() {
+        System.out.println("maxAge=" + userMapper.selectMaxAge());
+    }
+
+    @Test
+    public void testPagination() {
+        Page<User> page = new Page<>(1, 3);
+        userMapper.selectPage(page, null);
+        Assert.assertTrue("total should not be 0", page.getTotal() != 0);
+        for (User u : page.getRecords()) {
+            System.out.println(u);
+        }
+        Assert.assertEquals("pagination should be 3 per page", 3, page.getRecords().size());
     }
 
 }
