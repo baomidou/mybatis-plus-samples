ab1cb19babc1e2bfd72db1823e7e993d60100a43
miemie <wonderming@vip.qq.com>
2019-12-04 22:54:28 +0800

    test

--- mybatis-plus-sample-auto-fill-metainfo/src/main/java/com/baomidou/samples/metainfo/handler/MyMetaObjectHandler.java
+++ mybatis-plus-sample-auto-fill-metainfo/src/main/java/com/baomidou/samples/metainfo/handler/MyMetaObjectHandler.java
@@ -1,32 +1,30 @@
 package com.baomidou.samples.metainfo.handler;
 
 import org.apache.ibatis.reflection.MetaObject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 填充器
  *
  * @author nieqiurong 2018-08-10 22:59:23.
  */
+@Slf4j
 @Component
 public class MyMetaObjectHandler implements MetaObjectHandler {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(MyMetaObjectHandler.class);
-
     @Override
     public void insertFill(MetaObject metaObject) {
-        LOGGER.info("start insert fill ....");
-        //避免使用metaObject.setValue()
+        log.info("start insert fill ....");
         this.setFieldValByName("operator", "Jerry", metaObject);
     }
 
     @Override
     public void updateFill(MetaObject metaObject) {
-        LOGGER.info("start update fill ....");
+        log.info("start update fill ....");
         this.setFieldValByName("operator", "Tom", metaObject);
     }
 }
--- mybatis-plus-sample-auto-fill-metainfo/src/test/java/com/baomidou/samples/metainfo/AutoFillTest.java
+++ mybatis-plus-sample-auto-fill-metainfo/src/test/java/com/baomidou/samples/metainfo/AutoFillTest.java
@@ -1,37 +1,39 @@
 package com.baomidou.samples.metainfo;
 
-import com.baomidou.samples.metainfo.entity.User;
-import com.baomidou.samples.metainfo.mapper.UserMapper;
+import javax.annotation.Resource;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.baomidou.samples.metainfo.entity.User;
+import com.baomidou.samples.metainfo.mapper.UserMapper;
+
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * 自动填充测试
+ *
  * @author nieqiurong 2018-08-10 23:47:02.
  */
+@Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AutoFillTest {
-    
-    private static final Logger LOGGER = LoggerFactory.getLogger(AutoFillTest.class);
-    
-    @Autowired
+
+    @Resource
     private UserMapper userMapper;
-    
+
     @Test
-    public void test(){
-        User user = new User(null,"Tom",1,"tom@qq.com",null);
+    public void test() {
+        User user = new User(null, "Tom", 1, "tom@qq.com", null);
         userMapper.insert(user);
-        LOGGER.info("query user:{}",userMapper.selectById(user.getId()));
+        log.info("query user:{}", userMapper.selectById(user.getId()));
         User beforeUser = userMapper.selectById(1L);
-        LOGGER.info("before user:{}",beforeUser);
+        log.info("before user:{}", beforeUser);
         beforeUser.setAge(12);
         userMapper.updateById(beforeUser);
-        LOGGER.info("query user:{}",userMapper.selectById(1L));
+        log.info("query user:{}", userMapper.selectById(1L));
     }
 }
