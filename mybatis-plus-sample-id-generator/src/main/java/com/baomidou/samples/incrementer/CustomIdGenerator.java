b083e0ee4c926b74b3fcb8846b4a3d008f653ece
聂秋秋 <nieqiurong@163.com>
2019-12-30 16:27:52 +0800

    logging

--- mybatis-plus-sample-id-generator/src/main/java/com/baomidou/samples/incrementer/CustomIdGenerator.java
+++ mybatis-plus-sample-id-generator/src/main/java/com/baomidou/samples/incrementer/CustomIdGenerator.java
@@ -24,10 +24,13 @@ public class CustomIdGenerator implements IdentifierGenerator {
 
     @Override
     public Long nextId(Object entity) {
+        //可以将当前传入的class全类名来作为bizKey,或者提取参数来生成bizKey进行分布式Id调用生成.
+        String bizKey = entity.getClass().getName();
+        log.info("bizKey:{}", bizKey);
         MetaObject metaObject = SystemMetaObject.forObject(entity);
         String name = (String) metaObject.getValue("name");
         final long id = al.getAndAdd(1);
-        log.info("开始为: {},生成主键值为: {}", name, id);
+        log.info("为{}生成主键值->:{}", name, id);
         return id;
     }
 }
