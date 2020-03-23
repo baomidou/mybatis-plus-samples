aea16d55516d9b2819807f05340e3bdccb1e5209
miemie <wonderming@vip.qq.com>
2020-03-23 13:54:57 +0800

    test

---
+++ mybatis-plus-sample-sql-injector/src/main/java/com/baomidou/samples/injector/config/MySelectProvider.java
@@ -0,0 +1,12 @@
+package com.baomidou.samples.injector.config;
+
+/**
+ * @author miemie
+ * @since 2020-03-23
+ */
+public class MySelectProvider {
+
+    public String getSql(String sql) {
+        return sql;
+    }
+}
