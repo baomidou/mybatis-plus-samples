72dcc1d8c45e1d303db221e2c7f2e78338c416c3
yuxiaobin <179634696@qq.com>
2020-03-27 09:45:14 +0800

    add: h2 database code generation test h2 is CASE SENSITIVE by default

---
+++ mybatis-plus-sample-generator/src/main/java/com/baomidou/mybatisplus/samples/generator/GeneratorH2Application.java
@@ -0,0 +1,19 @@
+package com.baomidou.mybatisplus.samples.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 仅为了方便的生成h2数据库文件
+ *
+ * 代码生成，请参考test 目录下的H2CodeGenerationTest
+ */
+@SpringBootApplication
+public class GeneratorH2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeneratorH2Application.class, args);
+    }
+
+}
+
