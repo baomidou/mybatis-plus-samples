35e8c1d79b967636ca3456887fd8ec5f3be7b36f
miemie <wonderming@vip.qq.com>
2020-08-03 16:55:47 +0800

    page

--- mybatis-plus-sample-pagehelper/src/main/java/com/baomidou/mybatisplus/samples/pagehelper/MybatisPlusPageConfig.java
+++ mybatis-plus-sample-pagehelper/src/main/java/com/baomidou/mybatisplus/samples/pagehelper/MybatisPlusPageConfig.java
@@ -1,5 +1,6 @@
 package com.baomidou.mybatisplus.samples.pagehelper;
 
+import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
@@ -38,7 +39,7 @@ public class MybatisPlusPageConfig {
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.H2));
         return interceptor;
     }
 
--- mybatis-plus-sample-pagination/src/main/java/com/baomidou/mybatisplus/samples/pagination/config/MybatisPlusConfig.java
+++ mybatis-plus-sample-pagination/src/main/java/com/baomidou/mybatisplus/samples/pagination/config/MybatisPlusConfig.java
@@ -1,5 +1,6 @@
 package com.baomidou.mybatisplus.samples.pagination.config;
 
+import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
@@ -30,7 +31,7 @@ public class MybatisPlusConfig {
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.H2));
         return interceptor;
     }
 
