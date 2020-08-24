944c6e46322fa577792de07332d4534b9bc0c570
miemie <wonderming@vip.qq.com>
2020-08-24 18:48:29 +0800

    3.4.0

--- mybatis-plus-sample-pagination/src/main/java/com/baomidou/mybatisplus/samples/pagination/config/MybatisPlusConfig.java
+++ mybatis-plus-sample-pagination/src/main/java/com/baomidou/mybatisplus/samples/pagination/config/MybatisPlusConfig.java
@@ -1,12 +1,13 @@
 package com.baomidou.mybatisplus.samples.pagination.config;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * @author miemie
@@ -15,15 +16,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @MapperScan("com.baomidou.mybatisplus.samples.pagination.mapper")
 public class MybatisPlusConfig {
-
-//    /**
-//     * 老分页插件
-//     */
-//    @Bean
-//    public PaginationInterceptor paginationInterceptor() {
-//        // 开启 count 的 join 优化,只针对 left join !!!
-//        return new PaginationInterceptor().setCountSqlParser(new JsqlParserCountOptimize(true));
-//    }
 
     /**
      * 新的分页插件,一缓和二缓遵循mybatis的规则,需要设置 MybatisConfiguration#useDeprecatedExecutor = false 避免缓存出现问题
