3b027e1a889cf5f26ebd7f66c696312abae849e8
fxb <punkflyer@gmail.com>
2020-08-28 22:04:27 +0800

    替换StringUtils.isNotEmpty 为 isNotBlank

--- mybatis-plus-sample-generator/src/main/java/com/baomidou/mybatisplus/samples/generator/MysqlGenerator.java
+++ mybatis-plus-sample-generator/src/main/java/com/baomidou/mybatisplus/samples/generator/MysqlGenerator.java
@@ -41,7 +41,7 @@ public class MysqlGenerator {
         System.out.println(help.toString());
         if (scanner.hasNext()) {
             String ipt = scanner.next();
-            if (StringUtils.isNotEmpty(ipt)) {
+            if (StringUtils.isNotBlank(ipt)) {
                 return ipt;
             }
         }
