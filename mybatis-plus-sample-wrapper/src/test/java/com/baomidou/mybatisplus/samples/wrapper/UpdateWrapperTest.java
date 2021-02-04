1514b8577209e08cc036acb7a5643f8aa029a920
sdk <sundongkai2012@126.com>
2021-02-04 15:38:36 +0800

    修改操作的条件构造器示例

---
+++ mybatis-plus-sample-wrapper/src/test/java/com/baomidou/mybatisplus/samples/wrapper/UpdateWrapperTest.java
@@ -0,0 +1,66 @@
+package com.baomidou.mybatisplus.samples.wrapper;
+
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import com.baomidou.mybatisplus.samples.wrapper.entity.User;
+import com.baomidou.mybatisplus.samples.wrapper.mapper.UserMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author sundongkai
+ * @since 2021-02-04
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UpdateWrapperTest {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    /**
+     * UPDATE user SET age=?, email=? WHERE (name = ?)
+     */
+    @Test
+    public void tests() {
+
+        //方式一：
+        User user = new User();
+        user.setAge(29);
+        user.setEmail("test3update@baomidou.com");
+
+        userMapper.update(user,new UpdateWrapper<User>().eq("name","Tom"));
+
+        //方式二：
+        //不创建User对象
+        userMapper.update(null,new UpdateWrapper<User>()
+                .set("age",29).set("email","test3update@baomidou.com").eq("name","Tom"));
+
+    }
+
+    /**
+     * 使用lambda条件构造器
+     * UPDATE user SET age=?, email=? WHERE (name = ?)
+     */
+    @Test
+    public void testLambda() {
+
+        //方式一：
+        User user = new User();
+        user.setAge(29);
+        user.setEmail("test3update@baomidou.com");
+
+        userMapper.update(user,new LambdaUpdateWrapper<User>().eq(User::getName,"Tom"));
+
+        //方式二：
+        //不创建User对象
+        userMapper.update(null,new LambdaUpdateWrapper<User>()
+                .set(User::getAge,29).set(User::getEmail,"test3update@baomidou.com").eq(User::getName,"Tom"));
+
+    }
+
+
+}
