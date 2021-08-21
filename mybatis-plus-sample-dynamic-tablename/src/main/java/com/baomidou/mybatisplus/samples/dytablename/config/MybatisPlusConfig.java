f0158f1a2afd957d6a29213366b413f69ec6ca92
hubin <243194995@qq.com>
2021-08-21 16:49:50 +0800

    动态表名处理器调整演示

--- mybatis-plus-sample-dynamic-tablename/src/main/java/com/baomidou/mybatisplus/samples/dytablename/config/MybatisPlusConfig.java
+++ mybatis-plus-sample-dynamic-tablename/src/main/java/com/baomidou/mybatisplus/samples/dytablename/config/MybatisPlusConfig.java
@@ -1,13 +1,11 @@
 package com.baomidou.mybatisplus.samples.dytablename.config;
 
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.handler.TableNameHandler;
 import com.baomidou.mybatisplus.extension.plugins.inner.DynamicTableNameInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
@@ -23,22 +21,21 @@ public class MybatisPlusConfig {
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         DynamicTableNameInnerInterceptor dynamicTableNameInnerInterceptor = new DynamicTableNameInnerInterceptor();
-        HashMap<String, TableNameHandler> map = new HashMap<String, TableNameHandler>(2) {{
-            put("user", (sql, tableName) -> {
-                // 获取参数方法
-                Map<String, Object> paramMap = RequestDataHelper.getRequestData();
-                paramMap.forEach((k, v) -> System.err.println(k + "----" + v));
+        dynamicTableNameInnerInterceptor.setTableNameHandler((sql, tableName) -> {
+            // 获取参数方法
+            Map<String, Object> paramMap = RequestDataHelper.getRequestData();
+            paramMap.forEach((k, v) -> System.err.println(k + "----" + v));
 
-                String year = "_2018";
-                int random = new Random().nextInt(10);
-                if (random % 2 == 1) {
-                    year = "_2019";
-                }
-                return tableName + year;
-            });
-        }};
-        dynamicTableNameInnerInterceptor.setTableNameHandlerMap(map);
+            String year = "_2018";
+            int random = new Random().nextInt(10);
+            if (random % 2 == 1) {
+                year = "_2019";
+            }
+            return tableName + year;
+        });
         interceptor.addInnerInterceptor(dynamicTableNameInnerInterceptor);
+        // 3.4.3.2 作废该方式
+        // dynamicTableNameInnerInterceptor.setTableNameHandlerMap(map);
         return interceptor;
     }
 }
