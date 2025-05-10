889b67b0adaa134b6a0f467250bb1385e9b8532c
聂秋荣 <nieqiurong@163.com>
2025-05-11 00:04:23 +0800

    更新mysql示例配置.

--- mybatis-plus-sample-mysql/src/main/java/com/baomidou/mybatisplus/samples/mysql/config/MybatisPlusConfig.java
+++ mybatis-plus-sample-mysql/src/main/java/com/baomidou/mybatisplus/samples/mysql/config/MybatisPlusConfig.java
@@ -1,27 +1,21 @@
 package com.baomidou.mybatisplus.samples.mysql.config;
 
 import com.baomidou.mybatisplus.annotation.FieldFill;
-import com.baomidou.mybatisplus.core.MybatisConfiguration;
-import com.baomidou.mybatisplus.core.config.GlobalConfig;
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import com.baomidou.mybatisplus.core.injector.AbstractMethod;
 import com.baomidou.mybatisplus.core.injector.DefaultSqlInjector;
+import com.baomidou.mybatisplus.core.injector.ISqlInjector;
 import com.baomidou.mybatisplus.core.metadata.TableInfo;
-import com.baomidou.mybatisplus.extension.MybatisMapWrapperFactory;
 import com.baomidou.mybatisplus.extension.injector.methods.AlwaysUpdateSomeColumnById;
 import com.baomidou.mybatisplus.extension.injector.methods.InsertBatchSomeColumn;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
-import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
 import com.baomidou.mybatisplus.samples.mysql.injector.MysqlInsertAllBatch;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.type.JdbcType;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
-import javax.sql.DataSource;
 import java.util.List;
 
 /**
@@ -31,38 +25,22 @@ import java.util.List;
 @MapperScan("com.baomidou.mybatisplus.samples.mysql.mapper")
 public class MybatisPlusConfig {
 
-    @Bean("mybatisSqlSession")
-    public SqlSessionFactory sqlSessionFactory(DataSource dataSource, GlobalConfig globalConfig) throws Exception {
-        MybatisSqlSessionFactoryBean sqlSessionFactory = new MybatisSqlSessionFactoryBean();
-        /* 数据源 */
-        sqlSessionFactory.setDataSource(dataSource);
-        /* xml扫描 */
-        sqlSessionFactory.setMapperLocations(new PathMatchingResourcePatternResolver()
-            .getResources("classpath:/mapper/*.xml"));
-        /* 扫描 typeHandler */
-//        sqlSessionFactory.setTypeHandlersPackage("com.baomidou.mybatisplus.samples.mysql.type");
-        MybatisConfiguration configuration = new MybatisConfiguration();
-        configuration.setJdbcTypeForNull(JdbcType.NULL);
-        /* 驼峰转下划线 */
-        configuration.setMapUnderscoreToCamelCase(true);
-        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
-        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
-        mybatisPlusInterceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
-        sqlSessionFactory.setPlugins(mybatisPlusInterceptor);
-        /* map 下划线转驼峰 */
-        configuration.setObjectWrapperFactory(new MybatisMapWrapperFactory());
-        sqlSessionFactory.setConfiguration(configuration);
-        /* 自动填充插件 */
-        globalConfig.setMetaObjectHandler(new MysqlMetaObjectHandler());
-        sqlSessionFactory.setGlobalConfig(globalConfig);
-        return sqlSessionFactory.getObject();
+    @Bean
+    public MetaObjectHandler metaObjectHandler(){
+        return new MysqlMetaObjectHandler();
     }
 
     @Bean
-    public GlobalConfig globalConfig() {
-        GlobalConfig conf = new GlobalConfig();
-        conf.setDbConfig(new GlobalConfig.DbConfig().setColumnFormat("`%s`"));
-        DefaultSqlInjector logicSqlInjector = new DefaultSqlInjector() {
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        mybatisPlusInterceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        return mybatisPlusInterceptor;
+    }
+
+    @Bean
+    public ISqlInjector sqlInjector(){
+        return new DefaultSqlInjector() {
             /**
              * 注入自定义全局方法
              */
@@ -78,7 +56,6 @@ public class MybatisPlusConfig {
                 return methodList;
             }
         };
-        conf.setSqlInjector(logicSqlInjector);
-        return conf;
     }
+
 }
